71fa13ac020ef4bcceadeea2faa1af7faf3e10ea
Alan Gustavo <60892674+AlanGustav0@users.noreply.github.com>
2020-05-14 16:18:10 -0300

    Update comments

--- CalculoMedia/CalculoMedia.java
+++ CalculoMedia/CalculoMedia.java
@@ -2,6 +2,8 @@ import javax.swing.JOptionPane;
 
 class Main {
 
+    // Declaração de Variáveis
+
     static float Notas[][] = new float[3][2];
     static float media[][] = new float[3][2];
     static String alunos[] = new String[3];
@@ -9,12 +11,17 @@ class Main {
 
     public static void main(String[] args) {
 
+        // Primeiro laço de repetição (for) que receberá as notas dos alunos
         for (int i = 0; i < 3; i++) {
             JOptionPane.showMessageDialog(null, "Inserção dos dados do aluno " + (i + 1));
             for (int j = 0; j < 2; j++) {
 
+                // Laço de repetição (do while) repete a ação enquanto a condição não for
+                // satisfeita.
                 do {
                     Notas[i][j] = Float.parseFloat(JOptionPane.showInputDialog("Insira a nota da Prova " + (j + 1)));
+
+                    // Confição para verificar se o valor é menor que 0 ou maior que 10
                     if (Notas[i][j] < 0 || Notas[i][j] > 10) {
                         JOptionPane.showMessageDialog(null, "Valor inválido!");
                     }
@@ -22,15 +29,20 @@ class Main {
             }
         }
 
+        // Laço de repetição pata montar a estrutura de matriz que mostrará os
+        // resultados e fazer o cálculo da média
         for (int i = 0; i < 3; i++) {
             for (int j = 1; j < 2; j++) {
-                alunos[i] = "Média do " + (i + 1) + "º aluno: ";
-                media[i][j] = (Notas[i][j - 1] + Notas[i][j]) / 2;
-                mostrarMatriz += alunos[i] + media[i][j] + " " + " ";
+                alunos[i] = "Média do " + (i + 1) + "º aluno: "; // lista unidimencional que recebe o texto e o índice
+                                                                 // do aluno
+                media[i][j] = (Notas[i][j - 1] + Notas[i][j]) / 2; // Cálculo da média
+                mostrarMatriz += alunos[i] + media[i][j] + " " + " "; // Concatenando as informações na variável
+                                                                      // "mostraMatriz"
             }
             mostrarMatriz += "\n";
         }
 
+        // Mensagem mostrando o resultado final
         JOptionPane.showMessageDialog(null, "Abaixo temos a média dos alunos:" + "\n" + mostrarMatriz);
 
     }
