10d6a0ab4cff09b48e48de8b9c1ac434c1a84f9a
Alan Gustavo <60892674+AlanGustav0@users.noreply.github.com>
2020-06-18 12:55:04 -0300

    Create Fibonacci.java
    Program to create a Fibonacci sequence in Java

---
+++ Fibonnaci/Fibonacci.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+
+    public static void main(String[] args) {
+        int current = 1;
+        int previous = 1;
+        int next,number;
+        int count = 0;
+
+        Scanner fib = new Scanner(System.in);
+
+        System.out.println("Insert the number");
+        number = fib.nextInt();
+
+        while(count < number){
+
+            if(count == 0){
+                next = 1;
+            }else{
+                next = current + previous;
+                previous = current;
+                current = next;
+            }
+                System.out.print(next + "-");
+            count += 1;
+        }
+    }
+}
