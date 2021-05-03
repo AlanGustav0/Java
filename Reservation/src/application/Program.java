ac7709ecd81961c53a0b416c796874daa24b2da2
AlanGustav0 <alangustavo946@gmail.com>
2021-05-03 07:58:48 -0300

    Solução boa

--- Reservation/src/application/Program.java
+++ Reservation/src/application/Program.java
@@ -3,25 +3,22 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
 import javax.swing.JOptionPane;
-
 import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		int number = Integer.parseInt(JOptionPane.showInputDialog("Room number: "));
-		Date checkIn = sdf.parse(JOptionPane.showInputDialog("Check-in date (dd/MM/yyyy): "));
-		System.out.print("Check-out date (dd/MM/yyyy): ");
-		Date checkOut = sdf.parse(JOptionPane.showInputDialog("Check-out date (dd/MM/yyyy): "));
+		try {
+			int number = Integer.parseInt(JOptionPane.showInputDialog("Room number: "));
+			Date checkIn = sdf.parse(JOptionPane.showInputDialog("Check-in date (dd/MM/yyyy): "));
 
-		if (!checkOut.after(checkIn)) {
-			JOptionPane.showMessageDialog(null, "Error in reservation: Check-out date must be after check-in date");
-		} else {
+			Date checkOut = sdf.parse(JOptionPane.showInputDialog("Check-out date (dd/MM/yyyy): "));
+
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			JOptionPane.showMessageDialog(null, "Reservation: " + reservation);
 
@@ -31,12 +28,18 @@ public class Program {
 			checkOut = sdf.parse(JOptionPane
 					.showInputDialog("Enter data to update the reservation: \n" + "Check-out date (dd/MM/yyyy): "));
 
-			String error = reservation.updateDates(checkIn, checkOut);
-			if (error != null) {
-				JOptionPane.showMessageDialog(null, "Error in reservation: " + error);
-			} else {
-				JOptionPane.showMessageDialog(null, "Reservation: " + reservation);
-			}
+			reservation.updateDates(checkIn, checkOut);
+
+			JOptionPane.showMessageDialog(null, "Reservation: " + reservation);
+
+		} catch (ParseException e) {
+			JOptionPane.showMessageDialog(null, "Invalid date format!");
+
+		} catch (DomainException e) {
+			JOptionPane.showMessageDialog(null, "Error in Reservation: " + e.getMessage());
+		} catch (RuntimeException e) {
+			
+			JOptionPane.showMessageDialog(null, "Unespected error");
 		}
 
 	}
